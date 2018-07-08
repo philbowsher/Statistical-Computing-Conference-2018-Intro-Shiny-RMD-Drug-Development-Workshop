f402ea031447fe43dc292f7cbd2d350ec1faca1d
philbowsher <indyprb@gmail.com>
2018-07-08 21:25:44 +0000

    First Commit

---
+++ htmlwidgets/1_htmlwidgets_Leaflet_Starbucks/Leaflet_WA.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(leaflet) 
+library(magrittr)
+library(stringr)
+
+download.file("https://opendata.socrata.com/api/views/ddym-zvjk/rows.csv?accessType=DOWNLOAD",destfile="data.csv",method="libcurl")
+
+# Base R
+# starbucks1 <- read.csv("data.csv")
+
+# readr
+starbucks <- read_csv("data.csv")
+
+# is this needed? as_tibble(starbucks)
+
+# rename(starbucks, `StoreNumber` =  `Store Number` )
+
+#Make State a factor for later on
+#starbucks$State.f <- factor(starbucks$State)
+#is.factor(starbucks$State.f)
+
+class(starbucks)
+
+#View(starbucks2)
+
+starbucks
+
+# Base R
+# starbucks <- starbucks1[ which(starbucks1$State=='VA' & starbucks1$Country== 'US'), ]
+
+# Dplyr
+# Without pipes
+# starbucks_MO <- filter(starbucks, Country== 'US', State=='VA')
+
+starbucks_WA <- starbucks %>%
+  filter(Country== 'US', State=='WA')%>% 
+  select(Brand, `Store Number`, `Ownership Type`, City,	State,	Zip, Country, Coordinates,	Latitude,	Longitude, Name)
+
+
+leaflet() %>% addTiles() %>% setView(-122.338120, 47.613689, zoom = 12) %>% 
+
+addMarkers(data = starbucks_WA, lat = ~ Latitude, lng = ~ Longitude, popup = starbucks_WA$Name) %>%
+  addPopups(-122.338120, 47.613689, 'the <b> spot</b>')
